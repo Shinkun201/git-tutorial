ce42a01e31611b6a04ddc8ccb1532e3b5954db65
Shinkun <maivang2003@gmail.com>
2025-10-17 13:25:10 +0700

    nhánh bản đúng

--- test.py
+++ test.py
@@ -2,3 +2,7 @@
 print("Hello, world! Code chạy đúng nè.")
 print("Chào mừng bạn đến với Python!")
 print("Hãy cùng nhau học lập trình nhé!")
+print
+("Chúc bạn một ngày tốt lành!")
+print("Cảm ơn bạn đã sử dụng chương trình này.")("Hẹn gặp lại bạn trong các bài học tiếp theo!")
+print("Goodbye!")
